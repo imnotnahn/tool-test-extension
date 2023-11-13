a2d8f7bbb3fca7c5aa20001aba9dfd385193ec3a
nhanuit02 <20520665@gm.uit.edu.vn>
2023-11-13 14:35:42 +0700

    first commit

---
+++ js/autocheck.js
@@ -0,0 +1,5 @@
+
+document.getElementById("719-value").checked = true;
+document.getElementById("720-value").checked = true;
+document.getElementById("721-value").checked = true;
+document.getElementById("722-value").checked = true;
